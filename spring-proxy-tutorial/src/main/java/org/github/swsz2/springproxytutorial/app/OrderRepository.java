4000195c96314aa70d4a69adc350a80c5227a703
sungwonkim-muhayu <33013258+sungwonkim-muhayu@users.noreply.github.com>
2023-03-01 20:17:40 +0900

    Delete spring-proxy-tutorial/src/main/java/org/github/swsz2/springproxytutorial/app directory

--- spring-proxy-tutorial/src/main/java/org/github/swsz2/springproxytutorial/app/OrderRepository.java
+++
@@ -1,5 +0,0 @@
-package org.github.swsz2.springproxytutorial.app;
-
-public interface OrderRepository {
-  void save(final String itemId);
-}
--- spring-proxy-tutorial/src/main/java/org/github/swsz2/springproxytutorial/app/OrderService.java
+++
@@ -1,5 +0,0 @@
-package org.github.swsz2.springproxytutorial.app;
-
-public interface OrderService {
-  void orderItem(final String itemId);
-}
